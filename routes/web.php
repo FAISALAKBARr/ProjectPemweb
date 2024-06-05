c1d07d8a37ed02e7f0080bccd3554e0663ecb981
oodinc <masoodinc1@student.uns.ac.id>
2024-06-05 22:33:50 +0700

    edit

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScheduleController;
 
 Route::get('/', function () {
-    return view('pcmap');
+    return view('menu/pcmap');
 });
 
 Route::post('/schedules', [ScheduleController::class, 'store']);

536356edeab73e0e9124d1d406d673b2668050ce
FAISALAKBARr <faisalzogg022@gmail.com>
2024-06-07 20:15:48 +0700

    dne

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,5 @@ Route::get('/schedules/by-item-number', [ScheduleController::class, 'getByItemNu
 Route::get('/order', [MenuItemController::class, 'index']);
 Route::get('/menu-items', [MenuItemController::class, 'index']);
 Route::get('/menu-items/{id}', [MenuItemController::class, 'show']);
-terserah
-Route::get('/orders/by-item-id', [OrderController::class, 'byItemId']);
-Route::post('/orders', [OrderController::class, 'store']);
-
-Route::post('/tes', [ScheduleController::class, 'store']);
-Route::get('/tes/by-item-number', [ScheduleController::class, 'getByItemNumber']);
-Route::get('/tes', [MenuItemController::class, 'index']);
-Route::get('/wkwk', [MenuItemController::class, 'index']);
-Route::get('/wkwk/{id}', [MenuItemController::class, 'show']);
 Route::get('/orders/by-item-id', [OrderController::class, 'byItemId']);
 Route::post('/orders', [OrderController::class, 'store']);

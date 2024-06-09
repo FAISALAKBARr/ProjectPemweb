c350b0dfb1bc7a7eca2aae0c56c5b3ce6e2d3d25
Faisal Akbar <125114955+FAISALAKBARr@users.noreply.github.com>
2024-06-09 13:10:54 +0700

    Revert "menambahkan blocked dan IsAdmin"

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,8 @@ use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\GoogleAuthController;
-use App\Http\Controllers\Admin\UserController;
 
-Route::middleware(['auth', 'is_admin'])->group(function () {
-    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
-    Route::post('/admin/users/{user}/block', [UserController::class, 'block'])->name('admin.users.block');
-    Route::post('/admin/users/{user}/unblock', [UserController::class, 'unblock'])->name('admin.users.unblock');
-
+Route::middleware('auth')->group(function () {
     Route::get('/', function () {
         return view('menu.pcmap');
     });

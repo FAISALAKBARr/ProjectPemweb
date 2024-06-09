ef2247fcf1c930842b9ad2135746689238a1147f
oodinc <masoodinc1@student.uns.ac.id>
2024-06-09 10:37:33 +0700

    tambah login regist

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request; // Add this line
+
+class RegisterController extends Controller
+{
+    protected $redirectTo = '/';
+
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    public function create(Request $request) 
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    
+        User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    
+        return redirect()->route('login');
+    }
+    
+}

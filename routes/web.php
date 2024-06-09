6a0ba66163218954126a786f90f7f89daf54e6cf
RegaRohadi <regarohadi422@gmail.com>
2024-06-09 11:25:27 +0700

    menambahkan blocked dan IsAdmin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\MenuItemController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\Admin\UserController;
+
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
+    Route::post('/admin/users/{user}/block', [UserController::class, 'block'])->name('admin.users.block');
+    Route::post('/admin/users/{user}/unblock', [UserController::class, 'unblock'])->name('admin.users.unblock');
+});
 
 Route::get('/', function () {
     return view('menu/pcmap');
